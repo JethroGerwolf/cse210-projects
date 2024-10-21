04dcea4b84b0c9abf587135c0eb6f87c921f4ed6
Maximilian Laswell <laswellmaximilian@gmail.com>
2024-10-21 07:49:22 -0600

    learning 6

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        //shapes in a list
+        List<Shape> shapes = new List<Shape>();
+
+
+        Square square1 = new Square("blue", 5);
+        shapes.Add(square1);
+
+        Rectangle rec1 = new Rectangle("pink", 3, 6);
+        shapes.Add(rec1);
+
+        Circle cir1 = new Circle("red", 4);
+        shapes.Add(cir1);
+        
+        foreach (Shape i in shapes)
+        {
+
+            string color = i.GetColor();
+            double area = i.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }
